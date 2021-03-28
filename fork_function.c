0c94c47372c9dd61c9b593e997652350fe2cfc84
Siril Tharakan <tharakansiril@gmail.com>
2021-03-28 08:22:02 +0530

    Update fork_function.c

---
+++ fork_function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<unistd.h>
+
+int main(){
+  int id = fork();
+  if (id>0){
+    wait(NULL);
+  }
+  if(id == 0){
+    printf("Parent Process ID : %d\n", getppid());
+    printf("Child Process ID: %d\n", getpid());
+  }
+}
